bbdbf386b0b14f3183d2f5bb95caacc0bfe36579
Emanuel Bessa <41740085+Emanuelbessa@users.noreply.github.com>
2021-08-01 02:32:31 -0300

    fix(server): :bug: fix comic migration

--- src/config/knex/migrations/20210801012650_CreateComicFavTable.ts
+++ src/config/knex/migrations/20210801012650_CreateComicFavTable.ts
@@ -6,11 +6,12 @@ export async function up(knex: Knex) {
   return knex.schema.createTable(tableName, (ComicTable) => {
     ComicTable.specificType('cod_user_usr', 'char(36)')
       .notNullable()
-      .defaultTo(knex.raw('UUID()'));
+      .references('cod_user_usr')
+      .inTable('tb_marv_user');
     ComicTable.integer('cod_marvelid_com').notNullable();
     ComicTable.string('des_name_com').notNullable();
     ComicTable.string('des_thumbnail_com', 100).notNullable();
-    ComicTable.string('des_description_com', 60).notNullable();
+    ComicTable.string('des_description_com', 1000).notNullable();
     ComicTable.dateTime('dat_created_com').notNullable();
     ComicTable.primary(['cod_user_usr', 'cod_marvelid_com']);
   });
